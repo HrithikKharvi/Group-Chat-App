75e8b4432b06ece9301e11718f6658161352303b
Hrithik R <hrithikkharvi9@gmail.com>
2025-09-30 23:29:17 +0530

    added icon and added few styles

--- frontend-service/src/components/ChatGroup/GroupChatQueue.js
+++ frontend-service/src/components/ChatGroup/GroupChatQueue.js
@@ -2,12 +2,16 @@ import React from 'react';
 import cssModule from './GroupChatQueue.module.css';
 import ProfileImage from "../commonComponents/ProfileImage.js";
 import CommonKeyValue from "../commonComponents/CommonKeyValue.js";
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 const GroupChatQueue = (props) => {
 
     return <div className={cssModule.ChatGroupQueue}>
         <ProfileImage url={props.groupData.imgUrl}></ProfileImage>
         <span className={cssModule.groupName}>{props.groupData.groupName}</span>
+        <span>
+            <BsThreeDotsVertical size={24}></BsThreeDotsVertical>
+        </span>
     </div>
 }
 
